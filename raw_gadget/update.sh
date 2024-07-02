6af8d49656fe6f9cbcf6a6f5de37a2beedaff245
Andrey Konovalov <andreyknvl@gmail.com>
2024-07-02 22:43:27 +0200

    raw_gadget: update building instructions

---
+++ raw_gadget/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+
+set -eux
+
+REPO=https://git.kernel.org/pub/scm/linux/kernel/git/gregkh/usb.git/plain
+BRANCH=usb-next
+
+wget $REPO/drivers/usb/gadget/legacy/raw_gadget.c?h=$BRANCH -O raw_gadget.c
+wget $REPO/include/uapi/linux/usb/raw_gadget.h?h=$BRANCH -O raw_gadget.h
+
+# Fix up include names for local usage.
+git apply ./patches/include.patch
